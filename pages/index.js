61092bba8885c5b8b81510a6a8a56411db8a1962
chacha86 <taejincha0619@gmail.com>
2022-03-12 15:50:42 +0900

    폰트어썸 추가, 앱바 구현

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,30 @@
-import { Button } from "@mui/material";
+import { AppBar, Button, Toolbar } from "@mui/material";
 import Head from "next/head";
+import Link from "../src/Link";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>TODO</title>
+        <title>홈 | TODO</title>
         <meta name="description" content="할일관리" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Button variant="contained">버튼</Button>
-
-      <div className="w-[100px] h-[100px] bg-red-500"></div>
-      <ul>
-        <li>하하</li>
-        <li>하하</li>
-        <li>하하</li>
-        <li>하하</li>
-      </ul>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <Link href="/" noLinkStyle className="font-bold">
+            HAPPY NOTE
+          </Link>
+          <div className="flex-1 flex justify-end">
+            <Link href="/write" noLinkStyle>
+              <i class="fa-solid fa-pen"></i>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
     </>
   );
 }
